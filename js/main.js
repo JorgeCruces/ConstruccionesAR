ae898cc1b99c4304f2093dcfb6e01c24474cd5e6
Jorge <jorgecrucescolegio@gmail.com>
2019-01-25 00:29:59 -0300

    Se configuro el boton para celular

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
-    
-    $('.bars').on('click',function(){
+    $('#phone_menu').hide();
+    $('.bars i').on('click',function(){
         $('#phone_menu').slideToggle(200);
       
     })
