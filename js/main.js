9b793a1b4e16368b2a2f076fd75fc8d9040fb019
Jorge <jorgecrucescolegio@gmail.com>
2019-01-27 16:25:36 -0300

    Se agrego el mapa, se hicieron ajustes con el footer y se optimizo el diseño

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 $(function(){
+
+    // Navbar para celular
     $('#phone_menu').hide();
     $('.bars i').on('click',function(){
         $('#phone_menu').slideToggle(200);
       
-    })
+    });
+
+
+    // Mapa with LEAFLETJS 
+    
+    var map = L.map('mapa').setView([-33.3505969,-70.7185454], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([-33.3505969,-70.7185454]).addTo(map)
+    .bindPopup('Estamos AQUI!')
+    .openPopup();
+   
 
 });
